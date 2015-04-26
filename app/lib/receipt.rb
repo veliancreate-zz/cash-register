b3f98ff6239fa36468d128da7c9b6574e6a6e8bf
Julian Veling <julian_veling@hotmail.com>
2015-04-26 19:16:08 +0100

    receipt attributes acceptable

--- app/lib/receipt.rb
+++ app/lib/receipt.rb
@@ -3,6 +3,7 @@ require 'date'
 class Receipt
   attr_accessor :table_number, :tax_percentage
   attr_reader :date, :customers
+
   def set_date
     time = Time.now.strftime('%T')
     date = Time.now.strftime('%D')
@@ -12,5 +13,9 @@ class Receipt
   def customers=(customers = [])
     @customers = []
     customers.each { |customer| @customers.push(customer) }
+  end
+
+  def number_of_customers
+    @customers.count
   end
 end
--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -2,7 +2,7 @@ require_relative '../app/lib/receipt.rb'
 
 describe 'a complete receipt includes all the info required' do
   receipt = Receipt.new
-  it 'has date, customers, table_numbers, and tax_percentage' do
+  it 'has date, customers, table_number, and tax_percentage' do
     expect(receipt).to respond_to(:date)
     expect(receipt).to respond_to(:customers)
     expect(receipt).to respond_to(:table_number)
@@ -18,5 +18,14 @@ describe 'a complete receipt includes all the info required' do
     customers = %w(Max Sam Deb Sarah)
     receipt.customers = (customers)
     expect(receipt.customers).to eq(customers)
+  end
+  it 'number of customers is counted' do
+    customers = %w(Max Sam Deb Sarah)
+    receipt.customers = (customers)
+    expect(receipt.number_of_customers).to eq(4)
+  end
+  it 'tax precentage can be set and got' do
+    receipt.tax_percentage = 8.75
+    expect(receipt.tax_percentage).to eq(8.75)
   end
 end
