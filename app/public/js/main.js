5a76aa948d7099237a88dcc35d58355ea1430992
Julian Veling <julian_veling@hotmail.com>
2015-05-01 19:06:21 +0100

    readme update

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -2,12 +2,13 @@ $( document ).ready( function() {
   $( '.product' ).click( function( e ) {    
     e.preventDefault();
     text = $( this ).text();
+    
     $.post( '/', { product: text }, function( response ){
       arr = response.products
       $("ul#dynamic-list").empty();
       for(var i in arr) {
         var li = "<li>";
-        $("ul#dynamic-list").append(li.concat(arr[i].item), li.concat(arr[i].number));
+        $("ul#dynamic-list").append(li.concat(arr[i].number + ' x ' + arr[i].item));
       }
     });
   });
