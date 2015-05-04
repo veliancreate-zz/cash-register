3117db76c7ae3c568742afa45b8563d98983641e
Julian Veling <julian_veling@hotmail.com>
2015-05-04 21:51:48 +0100

    prior to refactor and partials

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,29 +1,9 @@
 $( document ).ready( function() {
-
   $( '.product' ).click( function( e ) {    
     e.preventDefault();
     var text = $( this ).text();
-    // function productUpdate(arr, li){
-    //   for(var i in arr) {
-    //     $("ul#dynamic-list").append(li.concat(arr[i].item + ' ' + arr[i].number + ' x ' + arr[i].item_price ));
-    //   }  
-    // }
-    // function receiptUpdate(receipt, li){
-    //   for (var property in receipt) {
-    //     if (receipt.hasOwnProperty(property)) {
-    //       $("ul#dynamic-costs-list").append(li.concat(receipt[property]))    
-    //     }
-    //   }
-    // }
     $.post( '/add_item', { product: text }, function( response ){
       window.location.replace('/');
-      // $("ul.dynamic-ul").empty();
-      // var li = "<li>";
-      // var arr = response.order.line_order;
-      // order = response.order;
-      // receipt = new Receipt(order);
-      // productUpdate(arr, li);
-      // receiptUpdate(receipt, li);  
     });
   });
 
